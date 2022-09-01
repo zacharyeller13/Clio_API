3a1984c502cd47d0888346ea9976694821a7d559
zacharyeller13 <zacharyeller@pm.me>
2022-09-01 15:27:50 -0500

    Finish /calendar_entries.json calls

--- Clio_API_Get_Users_And_Activities.py
+++ Clio_API_Get_Users_And_Activities.py
@@ -1,63 +1,106 @@
 import requests
 from requests.structures import CaseInsensitiveDict
+import csv
+
 from pprint import pprint as pp
+import time
 
 def set_headers() -> CaseInsensitiveDict:
+    """Sets and returns request headers necessary for Clio endpoints"""
 
     headers = CaseInsensitiveDict()
     headers["Host"] = "app.clio.com"
     headers["Content-Type"] = "application/x-www-form-urlencoded"
-    # headers["Authorization"] = ""
-    headers["Cookie"] = ""
+    headers["Authorization"] = ""
+    #headers["Cookie"] = ""
 
     return headers
 
-def get_calendar_entries(headers: CaseInsensitiveDict) -> requests.Response:
+def get_calendar_entries(url: str, headers: CaseInsensitiveDict) -> list[dict]:
+    """
+    Sends get request to /calendar_entries.json endpoint and returns the JSON of all entries.
+    Navigates paginated results if response includes a 'next' url.
+    """
+    # calendar_entries/####.json for specific entry
+    # e.g. 2524822026
 
-    url = "https://app.clio.com/api/v4/calendar_entries/2524822026.json"
+    # base url
+    # url = "https://app.clio.com/api/v4/calendar_entries.json"
+
+    entries = []
 
     resp = requests.get(url, headers=headers, params={
-        "fields": "id,etag,attendees,reminders,calendars,calendar_owner"
+        "fields": "id,attendees,calendar_owner",
+        "order" : "id(asc)"
         }
     )
 
     print(resp.status_code)
-    return resp
+    entries.extend(resp.json()['data'])
 
-# Calendar Response
+    while resp.json()['meta']['paging'].get('next'):
+        
+        url = resp.json()['meta']['paging'].get('next')
+        print(f"Requesting: {url}")
+        resp = requests.get(url, headers=headers)
+        print(resp.status_code)
+        print(f"X-RateLimit-Remaining: {resp.headers['X-RateLimit-Remaining']}")
 
-# pp(resp.json()['data'])
-# pp(resp.json()['data']['calendar_owner'])
+        if resp.headers.get('Retry-After'):
+            retry_period = int(resp.headers.get('Retry-After'))
+            print(f"Retry-After: {retry_period}")
 
-# for attendee in resp.json()['data']['attendees']:
-#     print(attendee)
+            for i in range(retry_period+1):
+                print(i, end='\r', flush=True)
+                time.sleep(1)
 
-def get_users(headers: CaseInsensitiveDict) -> requests.Response:
+        else:
+            entries.extend(resp.json()['data'])
 
-    url = "https://app.clio.com/api/v4/users.json"
+    return entries
 
-    resp = requests.get(url, headers=headers, params={
-        "fields": "id,name,default_calendar_id"
-        }
-    )
+def parse_calendar_entries(entries: list[dict]) -> list[dict]:
+    """Parses JSON response of calendar entries and returns a list of dicts for future processing"""
+    attendees = []
 
-    print(resp.status_code)
-    return resp
+    for entry in entries:
+        attendees.extend(
+            [{
+                'attendee_id': entry['attendees'][i]['id'],
+                'attendee_type': entry['attendees'][i]['type'],
+                'calendar_owner_id': entry['calendar_owner']['id'],
+                'activity_id': entry['id']
+            }
+            for i in range(len(entry['attendees']))]
+        )
 
-# Users Response
-# pp(users.json()['data'])
+    return attendees
 
-# for line in users.json()['data']:
-#     print(f"{line['id']} | {line['name']} | {line['default_calendar_id']}")
+def write_csv(attendees: list[dict], csv_file: str) -> None:
+
+    with open(csv_file, 'w', newline="") as csvfile:
+        fieldnames = attendees[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(attendees)
+
+def get_csv_filepath() -> str:
+    csv_file = input("Please enter the fully-qualified (absolute) path to the CSV file for this data: ")
+
+    return csv_file
 
 def main() -> None:
 
     headers = set_headers()
-    calendar_entries = get_calendar_entries(headers)
-    users = get_users(headers)
+    csv_file = get_csv_filepath()
 
-    pp(calendar_entries.json())
-    pp(users.json())
+    base_calendar_url = "https://app.clio.com/api/v4/calendar_entries.json"
 
-if __name__ == '__main__':
+    calendar_entries = get_calendar_entries(base_calendar_url, headers)
+    attendees = parse_calendar_entries(calendar_entries)
+
+    write_csv(attendees, csv_file)
+
+if __name__ == "__main__":
     main()
